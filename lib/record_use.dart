88011549db4554b4ba190c8f99da24df26b3acce
Moritz <mosum@google.com>
2024-06-05 15:05:56 +0200

    Fix missing exports

--- lib/record_use.dart
+++ lib/record_use.dart
@@ -2,7 +2,9 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+export 'src/arguments.dart' show Arguments, ConstArguments, NonConstArguments;
 export 'src/identifier.dart' show Identifier;
+export 'src/location.dart' show Location;
 export 'src/metadata.dart' show Metadata;
 export 'src/record_use.dart' show UsageRecord;
 export 'src/reference.dart' show CallReference, InstanceReference;
